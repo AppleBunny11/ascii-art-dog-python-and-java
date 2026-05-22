cf26bf0bda8d09ef4be552e0adbd88d34c552be5
AppleBunny11 <jasonescobar442@gmail.com>
2026-05-21 19:17:34 -0700

    Create Main,.java

---
+++ Main,.java
@@ -0,0 +1,13 @@
+// Java Code!
+public class Main
+{
+	public static void main(String[] args)
+	{
+		String dog = "     / \\__\n" +
+	             "    (    @\\___\n" +
+	             "    /         O\n" +
+	             "   /   (_____/\n" +
+	             "  /_____/   U\n";
+		System.out.println(dog);
+	}
+}
