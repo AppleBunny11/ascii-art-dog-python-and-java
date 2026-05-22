536de00429aab5a1e1ddf117fd323538b9ec444d
AppleBunny11 <jasonescobar442@gmail.com>
2026-05-21 19:19:16 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+# python code!
+def draw_ascii_dog():
+    dog = """
+      /\__
+     (    @\___
+     /         O
+    /    (_____/
+   /_______/   U   
+      """
+    print(dog)
+
+draw_ascii_dog()
